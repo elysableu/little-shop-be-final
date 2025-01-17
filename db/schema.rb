33332f1de4037f3789591a8a76d739991b4cd587
elysableu <elysabward@gmail.com>
2025-01-17 13:57:18 -0700

    Add coupons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_17_205501) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "code", null: false
+    t.float "discount", null: false
+    t.boolean "active", default: true, null: false
+    t.integer "num_of_uses", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_coupons_on_code", unique: true
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
