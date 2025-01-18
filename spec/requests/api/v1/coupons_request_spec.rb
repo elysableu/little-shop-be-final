062971f395c72db235bc2ddbce4e63b73e962186
elysableu <elysabward@gmail.com>
2025-01-17 19:58:41 -0700

    Add test skeleton for coupons spec

---
+++ spec/requests/api/v1/coupons_request_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "Coupon Endpoints", :type => :request do
+  let(:merchant) { Merchant.create!(name: "Lula Faye") }
+
+  describe "GET all coupons" do
+
+  end
+
+  describe "GET coupon by id" do
+
+  end
+
+  describe "Create coupon" do
+
+  end
+
+  describe "Update coupon status" do
+    describe "Active to Inactive" do
+
+    end
+
+    describe "Inactive to Active" do
+
+    end
+  end
+
+  # let(:coupon) {Coupon.create!(name: , code: , discount: , active: , num_of_uses: , merchant_id:)}
+end
