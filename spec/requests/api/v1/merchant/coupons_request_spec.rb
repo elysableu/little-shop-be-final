3bc9c9c74678149eeb5cce436aff8abcf4c4b660
elysableu <elysabward@gmail.com>
2025-01-18 15:52:31 -0700

    Add tests for merchants/coupons show endpoint

--- spec/requests/api/v1/merchant/coupons_request_spec.rb
+++ spec/requests/api/v1/merchant/coupons_request_spec.rb
@@ -3,17 +3,50 @@ require "rails_helper"
 describe "Coupon Endpoints", :type => :request do
   before(:each) do
     @merchant1 = create(:merchant)
-    @coupon1 = Coupon.create!(name: "New Years Discount", code: "NY2025", discount: 0.4, active: true, merchant_id: @merchant1.id);
-    @coupon2 = Coupon.create!(name: "Valentines Gift Sale", code: "FEB14LOVE", discount: 0.25, active: true, merchant_id: @merchant1.id);
-    @coupon3 = Coupon.create!(name: "Spring Celebration BOGO", code: "SPRBOGO25", discount: 0.5, active: false, merchant_id: @merchant1.id);
+    @coupon1 = Coupon.create!(name: "New Years Discount", code: "NY2025", discount: 0.4, active: true, merchant_id: @merchant1.id, num_of_uses: 2);
+    @coupon2 = Coupon.create!(name: "Valentines Gift Sale", code: "FEB14LOVE", discount: 0.25, active: true, merchant_id: @merchant1.id, num_of_uses: 1);
+    @coupon3 = Coupon.create!(name: "Spring Celebration BOGO", code: "SPRBOGO25", discount: 0.5, active: false, merchant_id: @merchant1.id, num_of_uses: 0);
   end
 
   describe "GET merchants coupon by id" do
+    it "should return a single coupon by ID" do
+      name = "New Years Discount"
+      code = "NY2025"
+      discount = 0.4
+      active = true
+      num_of_uses = 2
 
+      get "/api/v1/merchants/#{@merchant1.id}/coupons/#{@coupon1.id}"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:ok)
+      expect(json[:data]).to include(:id, :type, :attributes)
+      expect(json[:data][:id]).to eq("#{@coupon1.id}")
+      expect(json[:data][:type]).to eq("coupon")
+      expect(json[:data][:attributes][:name]).to eq(name)
+      expect(json[:data][:attributes][:code]).to eq(code)
+      expect(json[:data][:attributes][:discount]).to eq(discount)
+      expect(json[:data][:attributes][:active]).to eq(active)
+      expect(json[:data][:attributes][:num_of_uses]).to eq(num_of_uses)
+    end
+
+    it "should return 404 and error message when coupon is not found" do
+      test_id = 99999
+
+      get "/api/v1/merchants/#{@merchant1.id}/coupons/#{test_id}"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:not_found)
+      expect(json[:message]).to eq("Your query could not be completed")
+      expect(json[:errors]).to be_a Array
+      expect(json[:errors].first).to eq("Couldn't find Coupon with 'id'=#{test_id}")
+    end
   end
 
   describe "GET all merchants coupons" do
-    it "should return all items for a given merchant" do
+    it "should return all coupons for a given merchant" do
       get "/api/v1/merchants/#{@merchant1.id}/coupons"
 
       json = JSON.parse(response.body, symbolize_names: true)
