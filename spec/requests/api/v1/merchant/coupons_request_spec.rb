dedfa805de9f1b89e85c8a3e4684861289775f44
elysableu <elysabward@gmail.com>
2025-01-18 14:00:58 -0700

    Add test merchant and coupons

--- spec/requests/api/v1/merchant/coupons_request_spec.rb
+++ spec/requests/api/v1/merchant/coupons_request_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 describe "Coupon Endpoints", :type => :request do
-  let(:merchant) { Merchant.create!(name: "Lula Faye") }
+  before(:each) do
+    @merchant1 = create(:merchant)
+    @coupon1 = Coupon.create!(name: "New Years Discount", code: "NY2025", discount: 0.4, active: true);
+    @coupon2 = Coupon.create!(name: "Valentines Gift Sale", code: "FEB14LOVE", discount: 0.25, active: true);
+    @coupon3 = Coupon.create!(name: "Spring Celebration BOGO", code: "SPRBOGO25", discount: 0.5, active: false);
+  end
 
   describe "GET all merchants coupons" do
 
