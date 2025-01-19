2e45d0bfaaa07995d8b1147c9b8e94cc650e9cac
elysableu <elysabward@gmail.com>
2025-01-19 16:49:39 -0700

    Add test for filter coupons by active status

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -14,4 +14,16 @@ describe Coupon, type: :model do
     it { should belong_to :merchant }
     it { should have_many :invoices }
   end
+
+  describe 'class methods' do
+    it 'should filter a merchant\'s coupons by active status' do
+      merchant = create(:merchant)
+      coupon1 = Coupon.create(name: "New Years Discount", code: "NY2025", discount: 0.4, active: true, merchant_id: merchant.id, num_of_uses: 2);
+      coupon2 = Coupon.create(name: "Valentines Gift Sale", code: "FEB14LOVE", discount: 0.25, active: true, merchant_id: merchant.id, num_of_uses: 1);
+      coupon3 = Coupon.create(name: "Spring Celebration BOGO", code: "SPRBOGO25", discount: 0.5, active: false, merchant_id: merchant.id, num_of_uses: 3);
+
+      expect(merchant.coupons.filtered_by_active_status("active")).to eq([coupon1, coupon2])
+      expect(merchant.coupons.filtered_by_active_status("inactive")).to eq([coupon3])
+    end
+  end
 end
