5d2fe6380476fa1dd81d75344cd08a0859019245
elysableu <elysabward@gmail.com>
2025-01-20 17:55:10 -0700

    Update merchant controller and serializer to display coupon counts

--- db/migrate/20250120032130_add_columns_to_merchants.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToMerchants < ActiveRecord::Migration[7.1]
-  def change
-    add_column :merchants, :coupons_count, :integer, default: 0
-    add_column :merchants, :invoice_coupon_count, :integer, default: 0
-  end
-end
